3765fce779235096be328156f92196faf93a279e
Kranando <41551796+Kranando@users.noreply.github.com>
2018-11-18 18:52:34 +0200

    Yes

--- src/tree.rs
+++ src/tree.rs
@@ -1,32 +1,25 @@
 use piston_window::*;
 use opengl_graphics::{ GlGraphics, Texture };
-use rand::prelude::*;
+use rand::prelude::{Rng, thread_rng};
+use std::path::Path;
 
 pub struct Tree {
     pub x: f64,
     pub y: f64,
-    sprite: Option<Texture>
+    sprite: Texture
 }
 
 #[allow(dead_code)]
 impl Tree {
     pub fn new(i:usize) -> Tree {
         let mut rng = thread_rng();
-        Tree {x: (i as f64) * rng.gen_range(-600.0, 600.0), y: (i as f64) * rng.gen_range(-300.0, 300.0), sprite: None}
+        let treeimg = Texture::from_path(
+                        &Path::new("./assets/Tree.png"),
+                        &TextureSettings::new())
+                        .unwrap();
+        Tree {x: (i as f64) * rng.gen_range(-600.0, 600.0), y: (i as f64) * rng.gen_range(-300.0, 300.0), sprite: treeimg}
     }
     pub fn moar_trees(&self, gl: &mut GlGraphics, view: math::Matrix2d) {
-        let tree = rectangle::square(0.0, 0.0, 64.0);
-        let blue = [0.0, 0.0, 1.0, 1.0];
-        match self.sprite {
-            None => {
-                rectangle(blue, tree, view.trans(self.x, self.y).trans(-32.0, -32.0), gl)
-            }
-            Some(ref sprite) =>{
-                image(sprite, view.trans(self.x, self.y).trans(-32.0, -32.0), gl)
-            }
-        }
-    }
-    pub fn set_sprite(&mut self, sprite: Texture) {
-        self.sprite = Some(sprite);
+    image(&self.sprite, view.trans(self.x, self.y).trans(-32.0, -32.0), gl)
     }
 }
